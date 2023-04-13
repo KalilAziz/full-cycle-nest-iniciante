32105b6601ab72718cd1895ce7684473104db0e2
Kalil Aziz Santos Chami <kalilazizsantoschami@hotmail.com>
2023-04-13 09:39:11 -0300

    conhecendo o nestJs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,40 @@
+// path: src\app.controller.ts
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/* Decorator que define a classe como um controller, e recebe como parâmetro
+um objeto com as propriedades path e useValue. Apartir desse memento, ao
+importar o AppController dentro do nosso app.module.ts, o Nest irá reconhecer
+que o AppController é um controller e irá adicioná-lo ao array de controllers
+do módulo raiz da aplicação. Dentro do nosso controller, podemos definir
+métodos que serão mapeados para rotas HTTP, e que irão retornar uma resposta
+HTTP para o cliente.
+  Como por exemplo, o método getHello() abaixo, que será mapeado para a
+rota GET /, e que irá retornar uma string para o cliente. Está na rota /, pois
+o path do controller é vazio, e o path do método é vazio também. Caso coloquemos
+o path do controller como /app, e o path do método como /hello, o método
+getHello() será mapeado para a rota GET /app/hello.
+  */
+// @Controller('app')
 @Controller()
 export class AppController {
+  /* Por termos colocado o decorator @Injectable() no AppService, o Nest irá
+  reconhecer que o AppService é um provider e irá adicioná-lo ao array de
+  providers do módulo raiz da aplicação, E estará disponível para todos os
+  controllers que o importarem. O Nest só sabe que o AppService é um provider
+  porque o decoramos com o decorator @Injectable(), e colocamos ele dentro do
+  array de providers do módulo raiz da aplicação.
+  */
   constructor(private readonly appService: AppService) {}
 
+  /*Dentro do nosso controller, podemos definir métodos que serão mapeados
+  para rotas HTTP, e que irão retornar uma resposta HTTP para o cliente.
+  Como por exemplo, o método getHello() abaixo, que será mapeado para a
+  rota GET, e que irá retornar uma string para o cliente.
+  */
+
+  /* Método GET, mapeia para get, mas poderia ser qualquer método */
+  // @Get('hello')
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
+// path: src\app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/* Depois do main.ts, o próximo arquivo a ser analisado é o app.module.ts.
+Este arquivo é responsável por importar os módulos da aplicação, bem como
+exportar o módulo raiz da aplicação, que é o AppModule.
+  O módulo raiz da aplicação é responsável por importar os demais módulos
+da aplicação, bem como exportar os controllers e providers que serão
+utilizados por estes módulos.
+*/
+
 @Module({
+  /* Decorator que define a classe como um módulo, e recebe como parâmetro
+um objeto com as propriedades imports, controllers e providers.
+*/
+  // Array de módulos que serão importados pelo módulo raiz da aplicação.
   imports: [],
+  /* Array de controllers que serão exportados pelo módulo raiz da aplicação.
+  Todos os controllers importados por este módulo também serão exportados
+  pelo módulo raiz da aplicação.
+  */
   controllers: [AppController],
+  /* Array de providers que serão exportados pelo módulo raiz da aplicação.
+  Todos os providers importados por este módulo também serão exportados
+  pelo módulo raiz da aplicação. Além disso, todos os providers exportados
+  por este módulo também serão exportados por todos os módulos que o
+  importarem e estaram disponíveis para todos os controllers que o
+  importarem.
+  */
   providers: [AppService],
 })
 export class AppModule {}
